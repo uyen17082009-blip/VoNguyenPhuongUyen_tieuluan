c3ccbae0ee1f61a2b3f31301026f99805c1a0786
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-14 23:01:51 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   const [page, setPage] = useState("login");
 
   return (
     <BrowserRouter>
+
       <Header />
+      <Footer />
     </BrowserRouter>
   );
 }
