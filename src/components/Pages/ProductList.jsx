483a92cb094323c9f58499f314cb4f041013bff8
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-15 14:10:59 +0700

    Update ProductList.jsx

--- src/components/Pages/ProductList.jsx
+++ src/components/Pages/ProductList.jsx
@@ -1,30 +1,54 @@
 import { useState, useEffect } from 'react';
+import './ProductList.css'
 
-function ProductList() {
-  const [products, setProducts] = useState([]);
+import sp1Image from '../../img/lsp1/sp1.jpg';
+import sp2Image from '../../img/lsp1/sp2.jpg';
+import sp3Image from '../../img/lsp1/sp3.jpg';
+
+const imageMap = {
+  sp1: sp1Image,
+  sp2: sp2Image,
+  sp3: sp3Image
+};
+
+const ProductList = () => {
+  const [products, setProducts] = useStage([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Gọi file JSON từ thư mục public
-    fetch('/product.json') 
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data); // Lưu dữ liệu vào state
-      })
-      .catch((error) => console.error("Lỗi tải dữ liệu:", error));
+    const loadProducts = async () => {
+      try {
+        const reponse = await fetch('/products.json');
+        if(!reponse.ok) {
+          throw new Error('Không thể tải dữ liệu sản phẩm');
+        }
+        const data = await reponse.json();
+        const mappedProducts = data.map((item) => 
+    ({ ...item,
+        image: imageMap[item.imageKey] || item.image
+     }));        
+  setProducts(mappedProducts);
+      }catch (err) {
+        setError(err.message);
+      } finally{
+        setIsLoading(false);
+      }
+    };
+    loadProducts();
   }, []);
-
-  return (
-    <div>
-      <h2>Danh sách sản phẩm</h2>
-      <ul>
-        {products.map((item) => (
-          <li key={item.id}>
-            {item.name}
-          </li>
-        ))}
-      </ul>
+  if (isLoading) {
+    return <div className = "products-list-container">Lỗi: {error}</div>;
+  }
+  return(
+    <div className="product-list-container">
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+      ))}
+        
+      </div>
     </div>
-  );
-}
-
-export default ProductList;
+    );
+};
+export defaul ProductList;
