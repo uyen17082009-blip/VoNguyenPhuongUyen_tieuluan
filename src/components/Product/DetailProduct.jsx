b17542a79eee96b303df844fc1bc12bdeef1b09c
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-22 16:02:06 +0700

    Update DetailProduct.jsx

--- src/components/Product/DetailProduct.jsx
+++ src/components/Product/DetailProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useParams, useNavigate } from 'react-router-dom'
+import { useLocation, useParams, useNavigate } from 'react-router-dom';
 import './DetailProduct.css';
 import { imageMap } from '../../utils/productImages' ;
 const DetailProduct = () => {
