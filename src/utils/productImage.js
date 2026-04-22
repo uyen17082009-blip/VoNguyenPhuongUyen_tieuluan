4519c2936f323cbc22bb50a699b7693d90c250c9
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-22 14:06:15 +0700

    Update productImage.js

--- src/utils/productImage.js
+++ src/utils/productImage.js
@@ -1,1 +1,18 @@
 
+const imageModules = import.meta.glob('../img/*.{png,jpg,jpeg,webp,gì,svg}',{
+    eager:true
+});
+
+const baseName = (path) => {
+    const name = path.split('/').pop() || '';
+    return name.replace (/\.[^.]+$/ , '');
+};
+
+export const imageMap = Object.fromEntries(
+    Object.entries(imageModules).map(([path,mod])=>[baseName(path),mod.default])
+);
+
+export function resolveProductImage(imageKey){
+    if(imageKey == null || imageKey == '')return undefined;
+    return imageMap[imageKey];
+}
