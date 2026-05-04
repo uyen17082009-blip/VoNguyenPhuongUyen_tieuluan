b717cdf1f02255426436e05e774543e202cffbe9
uyen17082009-blip <uyen17082009@gmail.com>
2026-05-04 09:29:24 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-// main.jsx hoặc index.js
-import {  StrictMode } from 'react';
-import { createRoot  } from 'react-dom/client';
-
-import { BrowserRouter } from 'react-router-dom'; // Thêm dòng này
-import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
-  </StrictMode>
-);
+  </StrictMode>,
+)\
