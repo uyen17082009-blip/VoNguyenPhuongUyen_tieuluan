9f91bd03c029bc97f8e61f482bfc0e52921f98a3
uyen17082009-blip <uyen17082009@gmail.com>
2026-05-04 09:30:42 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer/Footer';
 import DetailProduct from './components/Product/DetailProduct';
 import ProductList from "./components/Product/ProductList";
 import ProductCard from "./components/Product/ProductCard";
-
+import Cart from "./components/Pages/Cart";
 
 function App() {
   const location = useLocation();
