3f10a5ade7a69d93bfcb9aa2558619bc3c6c6da8
uyen17082009-blip <uyen17082009@gmail.com>
2026-05-04 09:42:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import DetailProduct from './components/Product/DetailProduct';
 import ProductList from "./components/Product/ProductList";
-import ProductCard from "./components/Products/ProductCard";
+import ProductCard from "./components/Product/ProductCard";
 import Cart from "./components/Pages/Cart";
 
 function App() {
